283c14d5b15f0b08af2c85851094b49d6af7eea4
blackchildcodes <blackchildspace@gmail.com>
2024-02-27 17:23:55 +0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const IBMPlex = IBM_Plex_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+});
+
+export const metadata: Metadata = {
+  title: "SmartGenie",
+  description: "Advance Your Business With AI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#F76D22'}
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
